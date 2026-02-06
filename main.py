39592ca67c226c3deab6a8f7386e3fa4a087df3e
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2026-02-06 17:13:58 +0800

    add pytorch template

---
+++ main.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.multiprocessing as mp
+import torchaudio
+import torchvision
+
+# AI model parameter definitions
+DATASET_PATH = "../data/images"
+IMAGE_SIZE = (1024, 1024)
+CHANNELS = 3
+ACCURACY_THRESHOLD = 0.90
+
+def main():
+    ...
+
+if __name__ == "__main__":
+    main()
